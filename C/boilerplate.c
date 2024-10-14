e281c9f393efd0f17049fdb2c5759b03a625a712
Atul <anju.anju13981@gmail.com>
2024-10-14 11:43:48 +0530

    first commit

---
+++ C/boilerplate.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    printf("Name : Atul kumar \t Class : BCA 1A\n");
+    printf("**************************************\n");
+
+    
+}
---
+++ C/code/1.c
@@ -0,0 +1,13 @@
+// WAP to demonstrate the usage of escape sequences (\n, \t, \b, \\, \", \')
+#include <stdio.h>
+int main(){
+    printf("Name : Atul kumar \t Class : BCA 1A\n");
+    printf("**************************************\n");
+
+    printf("Going to next line \n");
+    printf("Creating Space bw 1 \t 2 \n");
+    printf("Removing last letter from CAT\b \n");
+    printf("Adding blackslash \\ \n");
+    printf("Adding  double quote \" Hello \" \n");
+    printf("Adding  single quote \' Hello \' ");
+}
